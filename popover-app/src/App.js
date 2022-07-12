0b99cb12242f729f8fc584f41b4007866ddfb9ae
SolbiShin <sbshin.dev@gmail.com>
2022-07-13 00:48:39 +0900

    popover기능 추가

--- popover-app/src/App.js
+++ popover-app/src/App.js
@@ -1,11 +1,35 @@
 import "./App.css";
+import React, { useRef, useState } from "react";
+import { usePopper } from "react-popper";
 
 function App() {
+  const referenceElement = useRef();
+  const popoverRef = useRef();
+  const [isOpen, setIsOpen] = useState(false);
+  const { styles, attributes } = usePopper(
+    referenceElement.current,
+    popoverRef.current
+  );
+  const onClickHandler = () => {
+    setIsOpen(!isOpen);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <button>Click to toggle popover</button>
-      </header>
+      <div ref={referenceElement} className="btn-section">
+        <button onClick={onClickHandler} type="button">
+          popover
+        </button>
+      </div>
+      <div
+        className={isOpen ? "popover" : "popover-hidden"}
+        ref={popoverRef}
+        style={styles.popper}
+        {...attributes.popper}
+      >
+        <p>
+          And here's some amazing content. It's very engaging. Right? <br />
+        </p>
+      </div>
     </div>
   );
 }
